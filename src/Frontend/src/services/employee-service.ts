a5930406fb5384b2ecd6ee5aaa6c96efef0f25cd
Fulvio DI Maio <86515090+fdm89@users.noreply.github.com>
2025-01-18 17:03:04 +0000

    adjusted employee service to handle filter request

--- src/Frontend/src/services/employee-service.ts
+++ src/Frontend/src/services/employee-service.ts
@@ -4,8 +4,16 @@ import { Employee } from "../types";
 class EmployeeService {
     
 
-    async getAllEmployees(): Promise <Employee[]> {
-        const response = await fetch('api/employees/list');
+    async getAllEmployees(firstName?: string, lastName?: string): Promise <Employee[]> {
+        let requestUrl = `api/employees/list`;
+        if(firstName){
+            requestUrl += `&FirstName=${firstName}`
+        }
+        if(lastName){
+            requestUrl += `&LastName=${lastName}`
+        }
+       
+        const response = await fetch(requestUrl);
         const results: Employee[] = await response.json();
         return results;
         
